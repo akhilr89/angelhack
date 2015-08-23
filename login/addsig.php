49f1eb169f4e955fafee3ba75a16b5109640d082
akhilr89 <akhilr04@gmail.com>
2015-08-23 11:33:41 +0530

    Create addsig.php

---
+++ login/addsig.php
@@ -0,0 +1,36 @@
+<?php
+include('session1.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>This is the Centralized Control Panel<?php echo ($login_session);?></title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <?php
+        include('../header.php');
+        ?>
+<div id="profile">
+<b id="welcome"><a href="profile1.php">Go Back<a></b>
+<b id="logout"><a href="logout.php">Log Out</a></b>
+</div>
+<br/>
+<center>
+<div id="content">
+
+    <center><p> 
+    <form method="post" action="adds.php">
+    <input id="lat" name="lat" placeholder="Latitude" type="text" required>
+    <br>
+    <input id="lon" name="lon" placeholder="Longitude" type="text" required>
+    <br>
+    <input name="submit" type="submit" value=" Add ">
+    </p></center>
+</div>
+</center>
+<?php
+		include('../footer.php');
+		?>
+</body>
+</html>
